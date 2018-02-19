d325b553a8da63974b0d298b9419d71e6ff2284d
MayuriVarade <mayurisupe@gmail.com>
2018-02-19 15:16:49 +0100

    Ready to deploy Version 1.0

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+    root "movies#index"
+    get "movies"     => "movies#index"
+    get "movies/:id" => "movies#show", as: "movie"
+    get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
